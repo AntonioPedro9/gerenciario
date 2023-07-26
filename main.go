098d56dfd86389068354561e05e2eecaf5467639
Antônio Pedro <antoniopedro.rs9@gmail.com>
2023-07-26 20:05:45 -0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"server/handlers"
 	"server/repositories"
 	"server/services"
-	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
@@ -15,15 +14,23 @@ import (
 )
 
 func main() {
+	// Create database connection
 	db, err := database.CreateDatabaseConnection()
 	if err != nil {
 		log.Fatal("Error connecting to database: ", err)
 	}
 	defer db.Close()
 
+	// Create database tables
 	err = database.CreateDatabaseTables(db)
 	if err != nil {
 		log.Fatal("Error creating database tables: ", err)
+	}
+
+	// Load environment variables
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file: ", err)
 	}
 
 	userRepository := repositories.NewUserRepository(db)
@@ -37,16 +44,6 @@ func main() {
 	router.HandleFunc("/users", userHandler.UpdateUser).Methods(http.MethodPut)
 	router.HandleFunc("/users/{id}", userHandler.DeleteUser).Methods(http.MethodDelete)
 
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file: ", err)
-	}
-
-	port, err := strconv.Atoi(os.Getenv("PORT"))
-	if err != nil {
-		log.Fatal("Error parsing PORT environment variable: ", err)
-	}
-
 	log.Info("Server started on port 8080")
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), router))
+	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
 }
