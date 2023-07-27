51ec38cdc09f3c1ffc2b9dc972aca57954d80018
Antônio Pedro <antoniopedro.rs9@gmail.com>
2023-07-27 16:35:20 -0300

    Update get_todays_date.go

--- utils/get_todays_date.go
+++ utils/get_todays_date.go
@@ -2,9 +2,6 @@ package utils
 
 import "time"
 
-func GetTodaysDate() string {
-	dateFormat := "01/02/2003"
-	formatedDate := time.Now().Format(dateFormat)
-
-	return formatedDate
+func GetTodaysDate() time.Time {
+	return time.Now()
 }
