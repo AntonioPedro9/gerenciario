c7dc0b84877c940e5ca66587d39d018ec5a773a5
Antônio Pedro <antoniopedro.rs9@gmail.com>
2023-09-19 11:02:42 -0300

    UUID is now generated directly as a string

--- utils/generate_uuid.go
+++ utils/generate_uuid.go
@@ -2,14 +2,9 @@ package utils
 
 import (
 	"github.com/google/uuid"
-	log "github.com/sirupsen/logrus"
 )
 
 func GenerateUUID() string {
-	id, err := uuid.NewRandom()
-	if err != nil {
-		log.Error("Failed to generate UUID")
-	}
-
-	return id.String()
+	id := uuid.NewString()
+	return id
 }
