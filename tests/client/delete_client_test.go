237085683cd7e37ae57fb2992723c587062c192b
Antônio Pedro <antoniopedro.rs9@gmail.com>
2023-10-04 19:49:56 -0300

    Create, list, update and delete client tests

---
+++ tests/client/delete_client_test.go
@@ -0,0 +1,77 @@
+package middlewares_test
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"server/database"
+	"server/handlers"
+	"server/models"
+	"server/repositories"
+	"server/services"
+	"server/utils"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func TestDeleteClient(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+
+	// setup layers
+	test_db := database.ConnectToDatabase()
+	userRepository := repositories.NewUserRepository(test_db)
+	clientRepository := repositories.NewClientRepository(test_db)
+	clientService := services.NewClientService(clientRepository)
+	clientHandler := handlers.NewClientHandler(clientService)
+
+	// create user that will delete the client
+	userID, _ := utils.GenerateUUID()
+	user := &models.User{
+		ID:       userID,
+		Name:     "Jonh Doe",
+		Email:    "jonhdoe@email.com",
+		Password: "password",
+	}
+	userRepository.Create(user)
+
+	// generate jwt token to authorize action
+	tokenString, _ := utils.GenerateToken(userID)
+
+	// create client that will deleted
+	client := &models.Client{
+		CPF:    "12345678910",
+		Name:   "Jonh Doe",
+		Email:  "jonhdoe@email.com",
+		Phone:  "(11) 9 2233-4455",
+		UserID: userID,
+	}
+	clientRepository.Create(client)
+
+	r.DELETE("/clients/:clientID", clientHandler.DeleteClient)
+
+	t.Run("Delete client", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodDelete, "/clients/1", nil)
+		request.AddCookie(&http.Cookie{Name: "Authorization", Value: tokenString})
+
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, request)
+
+		expectedStatus := http.StatusNoContent
+
+		if w.Code != expectedStatus {
+			t.Errorf("Expected status %d but got %d", expectedStatus, w.Code)
+		}
+	})
+
+	database.ClearTestDatabase(test_db)
+}
