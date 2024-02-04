6ad7b5c5bacde6efc54499802be0e029b257395c
Antônio Pedro <antoniopedro.rs9@gmail.com>
2024-02-04 03:48:04 -0300

    Named budget fields in the JSON

--- server/models/budget.go
+++ server/models/budget.go
@@ -7,14 +7,15 @@ import (
 )
 
 type Budget struct {
-	ID             uint `gorm:"primaryKey;autoIncrement"`
-	Price          float32
-	Date           time.Time `gorm:"default:CURRENT_TIMESTAMP"`
-	UserID         uuid.UUID `gorm:"not null"`
-	ClientID       uint      `gorm:"not null"`
-	ClientName     string    `gorm:"not null"`
-	BudgetServices []BudgetService
+	ID             uint            `gorm:"primaryKey;autoIncrement" json:"id"`
+	Price          float32         `json:"price"`
+	Date           time.Time       `gorm:"default:CURRENT_TIMESTAMP" json:"date"`
+	UserID         uuid.UUID       `gorm:"not null" json:"userID"`
+	ClientID       uint            `gorm:"not null" json:"clientID"`
+	ClientName     string          `gorm:"not null" json:"clientName"`
+	BudgetServices []BudgetService `json:"budgetServices"`
 }
+
 
 type CreateBudgetRequest struct {
 	Price      float32   `json:"price"`
