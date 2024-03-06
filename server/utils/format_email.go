d435ef2efe8c7fbbd82911ae6a827d62bef73db6
Antônio Pedro <antoniopedro.rs9@gmail.com>
2024-03-06 13:58:33 -0300

    Set email to lower case

--- server/utils/format_email.go
+++ server/utils/format_email.go
@@ -1,13 +1,18 @@
 package utils
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 func FormatEmail(email string) (string, error) {
 	if email == "" {
 		return email, nil
 	}
 
-	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	email = strings.ToLower(email)
+
+	emailRegex := regexp.MustCompile(`^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$`)
 	if !emailRegex.MatchString(email) {
 		return "", InvalidEmailError
 	}
